78ac4be829e756dbe5544f417db88389aae63599
CD Santiago <slashlos@gmail.com>
2017-07-04 16:48:17 -0400

    document updates (h2w), added displayImage()

--- Helium/Helium/Helium.swift
+++ Helium/Helium/Helium.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import QuickLook
 
 class PlayItem : NSObject {
     var name : String = k.item
@@ -33,36 +34,68 @@ class PlayItem : NSObject {
     }
 }
 
+internal struct Settings {
+    internal class Setup<T> {
+        private let key: String
+        private var setting: T
+        
+        init(_ userDefaultsKey: String, value: T) {
+            self.key = userDefaultsKey
+            self.setting = value
+        }
+        
+        var keyPath: String {
+            get {
+                return self.key
+            }
+        }
+        var `default`: T {
+            get {
+                if let value = UserDefaults.standard.object(forKey: self.key) as? T {
+                    return value
+                } else {
+                    // Sets existing setting if failed
+                    return self.setting
+                }
+            }
+        }
+        var value: T {
+            get {
+                return self.setting
+            }
+            set (value) {
+                self.setting = value
+                //  Inform all interested parties for this panel's controller only only
+                NotificationCenter.default.post(name: Notification.Name(rawValue: self.keyPath), object: nil)
+            }
+        }
+    }
+    
+    let autoHideTitle = Setup<Bool>("autoHideTitle", value: false)
+    let disabledFullScreenFloat = Setup<Bool>("disabledFullScreenFloat", value: false)
+    let opacityPercentage = Setup<Int>("opacityPercentage", value: 60)
+    let windowURL = Setup<URL>("windowURL", value: URL.init(string: "http://")!)
+    
+    // See values in HeliumPanelController.TranslucencyPreference
+    let translucencyPreference = Setup<HeliumPanelController.TranslucencyPreference>("rawTranslucencyPreference", value: .never)
+}
+
 class Document : NSDocument {
 
     var playlists: Dictionary<String, Any>
+    var settings: Settings
+    
+    func updateURL(to url: URL, ofType typeName: String) {
+        fileType = typeName
+        fileURL = url
+    }
     
     override init() {
         // Add your subclass-specific initialization here.
 
         playlists = Dictionary<String, Any>()
-        if let playArray = UserDefaults.standard.array(forKey: UserSettings.Playlists.keyPath) {
-            
-            for playlist in playArray {
-                let play = playlist as! Dictionary<String,AnyObject>
-                let items = play[k.list] as! [Dictionary <String,AnyObject>]
-                var list : [PlayItem] = [PlayItem]()
-                for playitem in items {
-                    let item = playitem as Dictionary <String,AnyObject>
-                    let name = item[k.name] as! String
-                    let path = item[k.link] as! String
-                    let time = item[k.time] as? TimeInterval
-                    let link = URL.init(string: path)
-                    let rank = item[k.rank] as! Int
-                    let temp = PlayItem(name:name, link:link!, time:time!, rank:rank)
-                    list.append(temp)
-                }
-                let name = play[k.name] as? String
-                
-                playlists[name!] = list
-            }
-        }
-
+        settings = Settings()
+        
         super.init()
     }
     
@@ -70,17 +103,35 @@ class Document : NSDocument {
         return false
     }
 
+    var displayImage: NSImage? {
+        get {
+            let size = NSMakeSize(CGFloat(kTitleNormal), CGFloat(kTitleNormal))
+
+            let tmp = QLThumbnailImageCreate(kCFAllocatorDefault, self.fileURL! as CFURL , size, nil)
+            if let tmpImage = tmp?.takeUnretainedValue() {
+                let tmpIcon = NSImage(cgImage: tmpImage, size: size)
+                return tmpIcon
+            }
+            else
+            {
+                return NSApp.applicationIconImage
+            }
+        }
+    }
+    
     override func makeWindowControllers() {
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
-        let controller = storyboard.instantiateController(withIdentifier: "HeliumController") as! HeliumPanelController
+        let controller = storyboard.instantiateController(withIdentifier: "HeliumController") as! NSWindowController
         self.addWindowController(controller)
         
         //  Close down any observations before closure
-        controller.window?.delegate = controller
+        controller.window?.delegate = controller as? NSWindowDelegate
     }
     
     override func data(ofType typeName: String) throws -> Data {
-        let data = try PropertyListSerialization.data(fromPropertyList: playlists, format: PropertyListSerialization.PropertyListFormat.xml, options: 0)
+        let data = try PropertyListSerialization.data(fromPropertyList: playlists,
+                                                      format: PropertyListSerialization.PropertyListFormat.xml,
+                                                      options: 0)
         if data.count > 0 {
             return data
         }
@@ -90,11 +141,14 @@ class Document : NSDocument {
     
     override func read(from data: Data, ofType typeName: String) throws {
         switch typeName {
-        case "internal":
-            let list = try! PropertyListSerialization.propertyList(from:data, options: [], format: nil) as! [String:Any]
-            if list.count > 0 {
-                Swift.print(String(format: "read %lu items", list.count))
-                //playlists = list
+        case "DocumentType":
+            let plist = try! PropertyListSerialization.propertyList(from:data,
+                                                                   options: [],
+                                                                   format: nil) as! Dictionary<String,AnyObject>
+            if plist.count > 0 {
+                playlists = plist[UserSettings.Playlists.keyPath] as! Dictionary<String, Any>
+                settings = plist[UserSettings.PlayPrefs.keyPath] as! Settings
+                break
             }
 
         default:
@@ -102,35 +156,96 @@ class Document : NSDocument {
         }
     }
 
-    convenience init(contentsOf: URL, ofType: String) throws {
+    convenience init(contentsOf url: URL, ofType typeName: String) throws {
         self.init()
         
-        switch ofType {
-        case "internal":
-            self.fileURL = contentsOf
-            self.fileType = ofType
+        switch typeName {
+        case "DocumentType":
+            if let dict = NSDictionary(contentsOf: url) {
+                if let lists = dict.value(forKey: UserSettings.Playlists.keyPath) {
+                    playlists = lists as! Dictionary<String, Any>
+                }
+                if let setup = dict.value(forKey: UserSettings.PlayPrefs.keyPath) {
+                    settings = setup as! Settings
+                }
+            }
+
+        case "h2w":
+            self.fileType = typeName
+            self.fileURL = url
             break
-            
+
         default:
-            Swift.print("nyi")
+            Swift.print("nyi \(typeName) -> \(url)")
             throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
         }
    }
     
     override func read(from url: URL, ofType typeName: String) throws {
         switch typeName {
-        case "internal":
-            self.fileURL = url
+        case "DocumentType":
+            if let playArray = UserDefaults.standard.array(forKey: UserSettings.Playlists.keyPath) {
+                
+                for playlist in playArray {
+                    let play = playlist as! Dictionary<String,AnyObject>
+                    let items = play[k.list] as! [Dictionary <String,AnyObject>]
+                    var list : [PlayItem] = [PlayItem]()
+                    for playitem in items {
+                        let item = playitem as Dictionary <String,AnyObject>
+                        let name = item[k.name] as! String
+                        let path = item[k.link] as! String
+                        let time = item[k.time] as? TimeInterval
+                        let link = URL.init(string: path)
+                        let rank = item[k.rank] as! Int
+                        let temp = PlayItem(name:name, link:link!, time:time!, rank:rank)
+                        list.append(temp)
+                    }
+                    let name = play[k.name] as? String
+                    
+                    playlists[name!] = list
+                }
+            }
+
             self.fileType = typeName
+            self.fileURL = url
             break
 
-        case "h2o":
+        case "h2w":
             let data = try Data.init(contentsOf: url)
             do {
                 try! self.read(from: data, ofType: typeName)
                 self.fileURL = url as URL
                 self.fileType = typeName
             }
+            break
+            
+        default:
+            Swift.print("nyi \(typeName)")
+            throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        }
+    }
+    
+    override func write(to url: URL, ofType typeName: String) throws {
+        switch typeName {
+        case "DocumentType":
+            var temp = [Dictionary<String,AnyObject>]()
+            for key in playlists.keys {
+                var list = Array<AnyObject>()
+                for playitem in playlists[key] as! [PlayItem] {
+                    let item : [String:AnyObject] = [k.name:playitem.name as AnyObject, k.link:playitem.link.absoluteString as AnyObject, k.time:playitem.time as AnyObject, k.rank:playitem.rank as AnyObject]
+                    list.append(item as AnyObject)
+                }
+                temp.append([k.name:key as AnyObject, k.list:list as AnyObject])
+            }
+            UserDefaults.standard.set(temp, forKey: UserSettings.Playlists.keyPath)
+            UserDefaults.standard.synchronize()
+            break
+            
+        case "h2w":
+            let dict = NSDictionary.init()
+            dict.setValue(playlists, forKey: UserSettings.Playlists.keyPath)
+            dict.setValue(settings, forKey: UserSettings.PlayPrefs.keyPath)
+            dict.write(to: url, atomically: true)
             break
             
         default:
