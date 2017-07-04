2a7d2de34fab88bd0159d8ba8ff0c35e2c5c6c94
CD Santiago <slashlos@gmail.com>
2017-07-04 10:37:15 -0400

    resolve url file alias

--- Helium/Helium/PlaylistViewController.swift
+++ Helium/Helium/PlaylistViewController.swift
@@ -74,6 +74,29 @@ extension NSURL {
     func compare(_ other: URL ) -> ComparisonResult {
         return (self.absoluteString?.compare(other.absoluteString))!
     }
+//  https://stackoverflow.com/a/44908669/564870
+    func resolvedFinderAlias() -> URL? {
+        if (self.fileReferenceURL() != nil) { // item exists
+            do {
+                // Get information about the file alias.
+                // If the file is not an alias files, an exception is thrown
+                // and execution continues in the catch clause.
+                let data = try NSURL.bookmarkData(withContentsOf: self as URL)
+                // NSURLPathKey contains the target path.
+                let rv = NSURL.resourceValues(forKeys: [ URLResourceKey.pathKey ], fromBookmarkData: data)
+                var urlString = rv![URLResourceKey.pathKey] as! String
+                if !urlString.hasPrefix("file://") {
+                    urlString = "file://" + urlString
+                }
+                return URL.init(string: urlString)
+            } catch {
+                // We know that the input path exists, but treating it as an alias
+                // file failed, so we assume it's not an alias file so return nil.
+                return nil
+            }
+        }
+        return nil
+    }
 }
 
 class PlaylistViewController: NSViewController,NSTableViewDataSource,NSTableViewDelegate {
