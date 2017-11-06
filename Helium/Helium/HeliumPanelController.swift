36b2fd2a82e1be9fa2648e21a15e798f0a6fc116
CD Santiago <slashlos@gmail.com>
2017-11-05 22:33:01 -0500

    latent new window per file handling

--- Helium/Helium/HeliumPanelController.swift
+++ Helium/Helium/HeliumPanelController.swift
@@ -359,7 +359,19 @@ class HeliumPanelController : NSWindowController,NSWindowDelegate {
         open.beginSheetModal(for: self.window!, completionHandler: { (response: NSModalResponse) in
             if response == NSModalResponseOK {
                 if let url = open.url {
-                    self.webViewController.loadURL(url:url)
+                    Swift.print("Opening \(open.url!)")
+                    self.webViewController.loadURL(url: url)
+/*                    do {
+                        let doc = try Document.init(contentsOf: url, ofType: url.pathExtension)
+                        
+                        if let hwc = (doc as NSDocument).windowControllers.first {
+                            hwc.window?.orderFront(self)
+                            (hwc.contentViewController as! WebViewController).loadURL(url: url)
+                        }
+                    } catch let error {
+                        print("*** Error open file: \(error.localizedDescription)")
+                    }
+*/
                 }
             }
         })
