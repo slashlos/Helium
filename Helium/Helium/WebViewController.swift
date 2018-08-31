7c3396202f9fcfe2705acee8bdf6659ed9099c7f
CD Santiago <slashlos@gmail.com>
2018-08-31 18:16:07 -0400

    added new windows option for contextual open

--- Helium/Helium/WebViewController.swift
+++ Helium/Helium/WebViewController.swift
@@ -675,6 +675,10 @@ for(var i=0; i< allLinks.length; i++)
         webView.magnification = 1
     }
 
+    fileprivate var openFilesInNewWindows : Bool = false
+    @objc fileprivate func openFilesInNewWindowsPress(_ sender: NSButton) {
+        openFilesInNewWindows = sender.state == NSOnState
+    }
     @IBAction func openFilePress(_ sender: AnyObject) {
         let window = self.view.window
         let open = NSOpenPanel()
@@ -684,12 +688,23 @@ for(var i=0; i< allLinks.length; i++)
         open.resolvesAliases = true
         open.canChooseFiles = true
         
+        //  Feature new window(s) when asked superseding user setting preference if no
+        let search = NSButton.init(checkboxWithTitle: "Open file(s) in new window(s)", target: self, action: #selector(WebViewController.openFilesInNewWindowsPress(_:)))
+        openFilesInNewWindows = false
+        open.accessoryView = search
+
         open.worksWhenModal = true
         open.beginSheetModal(for: window!, completionHandler: { (response: NSModalResponse) in
             if response == NSModalResponseOK {
                 let urls = open.urls
                 for url in urls {
-                    _ = self.appDelegate.doOpenFile(fileURL: url, fromWindow: window)
+                    if self.openFilesInNewWindows {
+                        self.appDelegate.openURLInNewWindow(url)
+                    }
+                    else
+                    {
+                        _ = self.appDelegate.doOpenFile(fileURL: url, fromWindow: window)
+                    }
                 }
             }
         })
